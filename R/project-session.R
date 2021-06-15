92f1d33ee0f81b724e9d07e5133be3f3f54cc354
Luke W. Johnston <lwjohnst@gmail.com>
2021-06-15 15:15:37 +0200

    Start of project

---
+++ R/project-session.R
@@ -0,0 +1,48 @@
+source(here::here("R/package-loading.R"))
+
+# Basics of R -------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos
+
+c("a", "b", "c")
+c(TRUE, FALSE)
+c(1, 2, 4)
+factor(c("high", "medium", "low"))
+
+head(CO2)
+colnames(CO2)
+str(CO2)
+summary(CO2)
+
+colnames(CO2)
+
+# Object names
+DayOne
+T <- FALSE
+c <- 9
+
+# Spacing
+x[, 1]
+x[, 1]
+mean (x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- feet * 12 + inches
+df$z
+x <- 1:10
+
+# Indenting and brackets
+if (y < 0 && debug) {
+    message("Y is negative")
+}
+
+
+
+
+
+
+
+
+
+
+
